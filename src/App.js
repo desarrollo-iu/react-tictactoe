7a5fffa4852ff895a88ecdafbc23add17266da17
Carlos Vega Meyer <carlosvega09@gmail.com>
2017-03-22 19:55:34 -0300

    Extracts calculateWinner into its own module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { calculateWinner } from './calculateWinner';
 
 function Square(props) {
   return (
@@ -96,25 +97,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-// ========================================
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
